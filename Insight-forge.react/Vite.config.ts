b459661ad1c1a8aca95bd687088aa364f4aafe0f
Yusuf <anime.accession0@gmail.com>
2026-09-09 21:11:22 +0530

    Update and rename Vote.config.ts to Vite.config.ts

--- Insight-forge.react/Vite.config.ts
+++ Insight-forge.react/Vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '//github.com/animeaccession0-arch/INSIGHT_FORGE', // 
   server: {
     port: 5173,
     open: true

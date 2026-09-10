066a0f5a2fbbb13af522a29f18e3b2bd5dc8b742
Yusuf <anime.accession0@gmail.com>
2026-09-11 00:08:49 +0530

    Update Vite.config.ts

--- Insight-forge.react/Vite.config.ts
+++ Insight-forge.react/Vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '//github.com/animeaccession0-arch/INSIGHT_FORGE', // 
+  base: '/INSIGHT_FORGE/',
   server: {
     port: 5173,
     open: true
